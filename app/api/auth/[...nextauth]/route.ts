46726b9f3ecda05789b3e6f4cd22b71b714c59f0
Daniel Jarrett <me@danieljarrett.dev>
2023-10-11 13:05:23 +0100

    move auth options to auth lib

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,5 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
-import { compare } from "bcrypt";
-import { RoleEnum, User } from "@prisma/client";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-
-      async authorize(credentials): Promise<User | Error> {
-        const { email, password } = credentials ?? {};
-
-        if (!email || !password) {
-          throw new Error("Missing username or password");
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email,
-          },
-        });
-
-        if (user?.role === RoleEnum.USER) {
-          throw new Error("Only admins can login");
-        }
-
-        if (!user || !(await compare(password, user.password))) {
-          throw new Error("Invalid username or password");
-        }
-
-        return user;
-      },
-    }),
-  ],
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,18 +2,51 @@ import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from 
 import type { NextAuthOptions } from "next-auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+import { compare } from "bcrypt";
+import { RoleEnum, User } from "@prisma/client";
 
-// You'll need to import and pass this
-// to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
-export const config = {
-  providers: [], // rest of your config
-} satisfies NextAuthOptions;
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials): Promise<User | Error> {
+        const { email, password } = credentials ?? {};
+
+        if (!email || !password) {
+          throw new Error("Missing username or password");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (user?.role === RoleEnum.USER) {
+          throw new Error("Only admins can login");
+        }
+
+        if (!user || !(await compare(password, user.password))) {
+          throw new Error("Invalid username or password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+};
 
 // Use it in server contexts
 export function auth(
   ...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []
 ) {
-  return getServerSession(...args, config);
+  return getServerSession(...args, authOptions);
 }
 
 export async function guard() {
