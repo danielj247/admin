5eb03f66fb5522619569eac7d5ef10ea4bd3cfc7
Daniel Jarrett <me@danieljarrett.dev>
2023-10-11 13:11:53 +0100

    ts-ignore authorize in auth lib

--- lib/auth.ts
+++ lib/auth.ts
@@ -15,6 +15,8 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
 
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       async authorize(credentials): Promise<User | Error> {
         const { email, password } = credentials ?? {};
 
